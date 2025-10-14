02c6ffc9be55bb05c68bdc95e63fc63a44b421c6
Nux Gajurel <gajurelnux30@gmail.com>
2025-10-14 12:16:24 +0545

    done..

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
-
+import Main from '../../public/Img/ahh.jpg'
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+
+     <div className=''>
+      <h1 className='text-3xl mt-7 ml-4 text-shadow-2xs'> 🌿About BloomHaven</h1>
+
+      <div className=''>
+        <div>
+<p className='mt-6 ml-3 mr-1'>
+  Welcome to BloomHaven, your one-stop destination for discovering beauty, creativity, and comfort in every product.
+At BloomHaven, we believe shopping should be more than just a transaction — it should be an experience filled with inspiration and joy.
+
+From timeless fashion pieces to elegant home essentials, every item in our collection is carefully chosen to help you express your unique style and elevate your everyday life.
+Our goal is to make high-quality, thoughtfully designed products accessible to everyone, while delivering an exceptional online shopping experience.
+
+We’re passionate about combining modern design, trusted quality, and customer satisfaction — because your happiness is what makes BloomHaven bloom. 🌸
+</p>
+        </div>
+
+   <div className=''>
+   <div className='flex justify-center items-center'>
+    <img src={Main} alt="" className='lg:w-340 lg:h-120 mt-8'/>
+  </div>
+
+
+  <div className='mt-25 ml-4'>
+    <h1 className='text-2xl text-shadow-2xs'>💚 Our Promise</h1>
+   <li className='mt-3 ml-4'>Trusted quality and authentic designs</li>
+   <li className='ml-4'>Smooth, secure, and fast shopping experience</li>
+   <li className='ml-4'>Customer-first support that truly cares</li>
+   <li className='ml-4'>Smooth, Secure & Fast Experience</li>
+   <li className='ml-4'>
+
+ Sustainability & Responsibility</li>
+<li className='ml-4'>We believe that beauty and responsibility</li>
+
+  </div>
+
+
+   </div>
+
+
+       </div>
+
+  
+     </div>
+
+
+
+
+
+
+
+    </div>
   )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,75 @@
-import React from 'react'
-import Main from '../../public/Img/main.png'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Main from "../../public/Img/main.png";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div>
-        <div className='lg:flex lg:flex-row lg:items-start space-y-3 mb-6 lg:mb-0 space-x-10 m-3'>
+      <div className="lg:flex lg:flex-row lg:items-start space-y-3 mb-6 lg:mb-0 space-x-10 m-3">
         <div>
-        <img src={Main} alt="" className='h-40 w-40'/>
-        
+          <img src={Main} alt="" className="h-47 w-40" />
         </div>
 
-         <div className='grid grid-rows-4 space-x-3 mt-9'>
-       <Link to="/home" className="hover:text-pink-600 transition-colors text-black">Home</Link>
-        <Link to="/product" className="hover:text-pink-600 transition-colors">Product</Link>
-        <Link to="/about" className="hover:text-pink-600 transition-colors">About</Link>
-        <a href="https://github.com/NuxGajurel/BloomHaven-e-commerce-" className="hover:text-pink-600 transition-colors">GitHub Repo</a>
-
-      
-   
+        <div className="grid grid-rows-4 space-x-3 mt-9">
+           <h2>Pages</h2>
+          <Link
+            to="/home"
+            className="hover:text-pink-600 transition-colors text-black"
+          >
+            Home
+          </Link>
+          <Link to="/product" className="hover:text-pink-600 transition-colors">
+            Product
+          </Link>
+          <Link to="/about" className="hover:text-pink-600 transition-colors">
+            About
+          </Link>
+          <a
+            href="https://github.com/NuxGajurel/BloomHaven-e-commerce-"
+            className="hover:text-pink-600 transition-colors"
+          >
+            GitHub Repo
+          </a>
+        </div>
+        <div className="grid grid-rows-4 space-x-3 lg:m-9 mt-2">
+          <h2>Connect</h2>
+          <a
+            href="https://github.com/NuxGajurel"
+            className="hover:text-pink-600 transition-colors"
+          >
+            GitHub
+          </a>
+          <a
+            href="https://www.instagram.com/nuxgajurel/"
+            className="hover:text-pink-600 transition-colors"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.linkedin.com/in/nux-gajurel-355962348/"
+            className="hover:text-pink-600 transition-colors"
+          >
+            Linkedin
+          </a>
+          <a
+            href="nuxgajurel46@gmail.com"
+            className="hover:text-pink-600 transition-colors"
+          >
+            G-mail
+          </a>
+        </div>
       </div>
-       <div className='grid grid-rows-4 space-x-3 lg:m-9 mt-2'>
-         <a href="https://github.com/NuxGajurel" className="hover:text-pink-600 transition-colors">GitHub</a>
-         <a href="https://www.instagram.com/nuxgajurel/" className="hover:text-pink-600 transition-colors">Instagram</a>
-         <a href="https://www.linkedin.com/in/nux-gajurel-355962348/" className="hover:text-pink-600 transition-colors">Linkedin</a>
-         <a href="nuxgajurel46@gmail.com" className="hover:text-pink-600 transition-colors">G-mail</a>
-         </div>
-       
-       
-        </div>
-     
-      <div className='mt-2 m-3'>
-            <p className='flex items-center justify-center'> Made By  <a href="https://github.com/NuxGajurel" className='text-blue-500'>@NuxGajurel</a></p>
-         </div>
-    </div>
-  )
-}
 
-export default Footer
+      <div className="mt-2 m-3">
+        <p className="flex items-center justify-center">
+          {" "}
+          Made By{" "}
+          <a href="https://github.com/NuxGajurel" className="text-blue-500">
+            @NuxGajurel
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
