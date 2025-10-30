3853b10c0863d63791dda8521db7bdc68bb80866
Nux Gajurel <gajurelnux30@gmail.com>
2025-10-30 18:46:26 +0545

    commit home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
           <Link to="/product">
             <div
               key={flowerProduct.id}
-              className="rounded p-1 bg-white shadow-md items-center flex justify-center hover:border "
+              className="rounded p-1 bg-white shadow-md items-center flex justify-center "
             >
               <img
                 src={flowerProduct.url}
